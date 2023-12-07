71bb825901f210baf2f98d9745d0901f0a7b2228
zzoo <87777405+zzoopro@users.noreply.github.com>
2023-12-07 22:55:49 +0900

    [level unrated] Title: 연속된 수의 합, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120923. 연속된 수의 합/연속된 수의 합.js
+++ 프로그래머스/unrated/120923. 연속된 수의 합/연속된 수의 합.js
@@ -1,9 +1,8 @@
 function solution(num, total) {
+    const start = (total / num) - (num / 2) + (1 / 2)    
     let result = []
-    const mid = total / num
-    let start = Number.isInteger(mid) ? Math.floor(mid - Math.floor(num / 2)) : Math.floor(mid - Math.floor(num / 2)) + 1
     for(let i = start; i < start + num; i++) {
-        result.push(i)   
-    }    
+        result.push(i)
+    }
     return result
 }
