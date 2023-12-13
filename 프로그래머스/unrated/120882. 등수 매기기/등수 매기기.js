2d315ebfc0568a813b179ce4e8b2e598aee1cdf8
zzoo <87777405+zzoopro@users.noreply.github.com>
2023-12-13 22:46:51 +0900

    [level unrated] Title: 등수 매기기, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/unrated/120882. 등수 매기기/등수 매기기.js
@@ -7,9 +7,10 @@ function solution(score) {
     // 평균 점수를 기준으로 정렬
     averages.sort((a, b) => b.average - a.average);
 
-    // 등수 매기기
-    const ranks = new Array(score.length).fill(0);
+    let ranks = []
     let rank = 1;
+
+    
     for (let i = 0; i < averages.length; i++) {
         // 이전 학생과 평균 점수가 같으면 같은 등수 부여
         if (i > 0 && averages[i].average === averages[i - 1].average) {
@@ -19,6 +20,5 @@ function solution(score) {
         }
         rank++;
     }
-
-    return ranks;
+    return ranks
 }
