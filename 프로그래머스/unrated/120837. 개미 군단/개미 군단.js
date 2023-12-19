b8065d73522eeee9ac9e45d06cc45b17e3411373
zzoo <87777405+zzoopro@users.noreply.github.com>
2023-12-20 00:07:04 +0900

    [level unrated] Title: 개미 군단, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120837. 개미 군단/개미 군단.js
@@ -0,0 +1,16 @@
+function solution(hp) {
+    let count = 0
+    while(hp >= 5) {
+        hp -= 5
+        count += 1
+    }
+    while(hp >= 3) {
+        hp -= 3
+        count += 1
+    }
+    while(hp >= 1) {
+        hp -= 1
+        count += 1
+    }
+    return count
+}
