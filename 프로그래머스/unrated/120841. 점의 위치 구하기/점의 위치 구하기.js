de8c13d300e0e245f4eb3fc9aac74facf388dad0
zzoo <87777405+zzoopro@users.noreply.github.com>
2023-12-19 22:52:57 +0900

    [level unrated] Title: 점의 위치 구하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -0,0 +1,16 @@
+function solution(dot) {
+    const [x, y] = dot
+    if(x > 0) {
+        if(y > 0) {
+            return 1
+        } else {
+            return 4
+        }
+    } else {
+        if(y > 0) {
+            return 2
+        } else {
+            return 3
+        }
+    }
+}
