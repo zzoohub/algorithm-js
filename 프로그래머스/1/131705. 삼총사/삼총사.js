f046a487ee0ee466d41a3ec7c39d1062ce47debd
zzoo <87777405+zzoopro@users.noreply.github.com>
2023-12-25 20:51:14 +0900

    [level 1] Title: 삼총사, Time: 3.86 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -1,18 +1,20 @@
 function solution(number) {
     const combi = combinations(number, 3)
+    console.log(`combination: [${combi}]`)
     return combi.filter((arr) => arr.reduce((acc, cur) => acc + cur, 0) === 0).length
 }
 
-function permutations(arr) {
-    if (arr.length <= 1) return [arr];
-    return arr.flatMap((item, i) => 
-        permutations(arr.slice(0, i).concat(arr.slice(i + 1))).map(p => [item, ...p])
-    );
-}
+// function permutations(arr) {
+//     if (arr.length <= 1) return [arr];
+//     return arr.flatMap((item, i) => 
+//         permutations(arr.slice(0, i).concat(arr.slice(i + 1))).map(p => [item, ...p])
+//     );
+// }
 
 function combinations(arr, n) {
     if (n === 0) return [[]];
     if (n > arr.length) return [];
+    console.log(`arr: [${arr}], n: ${n}`)
     return arr.flatMap((item, i) => 
         combinations(arr.slice(i + 1), n - 1).map(c => [item, ...c])
     );
