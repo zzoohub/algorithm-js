3533082184a00413274c317f722be96319eae285
zzoo <87777405+zzoopro@users.noreply.github.com>
2024-01-02 01:37:36 +0900

    [level 1] Title: 콜라 문제, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/README.md
+++ 프로그래머스/1/132267. 콜라 문제/README.md
@@ -1,10 +1,10 @@
 # [level 1] 콜라 문제 - 132267 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132267) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132267#) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.04 ms
+메모리: 33.5 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 2일 1:36:46
+2024년 1월 2일 1:37:35
 
 ### 문제 설명
 
--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -1,12 +1,14 @@
+function solution(changeRatio, b, initialEmpties) {
+    
+    let empties = initialEmpties
+    let currentCola = 0;
+    let totalCola = 0
 
-function solution(a, b, n) {
-    let totalCola = 0;
-
-    while (n >= a) {
-        let newCola = Math.floor(n / a) * b; // 새로 받을 콜라의 수
-        totalCola += newCola; // 총 콜라 수에 추가
-        n = newCola + (n % a); // 새로운 콜라와 남은 빈 병으로 업데이트
+    while(empties >= changeRatio) {
+        const changable = Math.floor(empties / changeRatio) * b
+        empties = changable + (empties % changeRatio)
+        totalCola += changable      
     }
-
-    return totalCola;
+    
+    return totalCola
 }
