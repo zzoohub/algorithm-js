96153456551c896c9ffa21849afce0042860dc37
zzoo <zzoo.origin@gmail.com>
2025-05-06 23:45:37 +0900

    [level 1] Title: 둘만의 암호, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -1,31 +1,16 @@
 function solution(s, skip, index) {
-   function getNextCharCode(cur, jump) {
-  // 'a' 코드(97)를 기준으로 26글자 안에서만 순회하도록
-  return ( (cur - 97 + jump) % 26 ) + 97;
-} 
-    function getValue(char) {
-        const curCharCode = char.charCodeAt()
-        let skipCount = 0;
-        let count = 1;    
-        let value = "";        
-        while(count <= index) {            
-            const nextCharCode = getNextCharCode(curCharCode, count + skipCount)                      
-            const nextValue = String.fromCharCode(nextCharCode)                 
-            if(skip.includes(nextValue)) {
-                skipCount++
-                continue
-            }            
-            if(count === index) {
-                return nextValue;
-            }
-            count++
-        }        
+  const skipSet = new Set(skip);  
+  return [...s].map(char => {
+    let code = char.charCodeAt(0);
+    let moved = 0;
+
+    while (moved < index) {
+      // 다음 문자로 이동 (z → a)
+      code = code === 122 ? 97 : code + 1;
+      // skip 에 없으면 이동 카운트
+      if (!skipSet.has(String.fromCharCode(code))) moved++;
     }
-    
-    let result = ""
-    for(let i = 0; i < s.length; i++) {                  
-       result += getValue(s[i])               
-    }    
-    
-    return result
+
+    return String.fromCharCode(code);
+  }).join('');
 }
