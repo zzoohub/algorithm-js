462b928f86b24a70f46ea0d4389ea52510a3529e
zzoo <zzoo.origin@gmail.com>
2025-05-31 15:51:12 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 6.36 ms, Memory: 36.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -0,0 +1,22 @@
+function solution(s) {
+  let count = 0;
+  let deletedZeroCount = 0;
+
+  function calculate(str) {
+    if (str === "1") return;
+
+    const originalLen = str.length;
+    const filtered = str.replace(/0/g, ""); // "0" 제거한 문자열
+    const filteredLen = filtered.length;
+
+    deletedZeroCount += originalLen - filteredLen;
+    count += 1;
+
+    const nextStr = filteredLen.toString(2); // 이진수 변환
+    calculate(nextStr);
+  }
+
+  calculate(s);
+
+  return [count, deletedZeroCount];
+}
