6511809e080add5bc03d95202b2187c2783dc0f5
zzoo <zzoo.origin@gmail.com>
2025-07-17 08:58:05 +0900

    [level 2] Title: 피보나치 수, Time: 3.90 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -1,15 +1,12 @@
 function solution(n) {
     const MOD = 1234567    
  
-    let prev2 = 0;
-    let prev1 = 1;
-    
-    // F(2)부터 F(n)까지 순차적으로 계산
+    let a = 0;
+    let b = 1;
+        
     for (let i = 2; i <= n; i++) {
-        let current = (prev1 + prev2) % MOD;
-        prev2 = prev1;
-        prev1 = current;
+        [a, b] = [b, (b + a) % MOD]
     }
     
-    return prev1;
+    return b;
 }
